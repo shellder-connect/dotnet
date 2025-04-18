68b09452d06aafa3ab9fa14443006eef631a35a7
Claudio Bispo <claudio.bispo@ezzeseguros.com.br>
2025-04-18 14:18:50 -0300

    FEAT - Finalização dos teste com Usuário Service

--- Tests/Project.Tests/UnitTests/Services/UsuarioServiceTests.cs
+++ Tests/Project.Tests/UnitTests/Services/UsuarioServiceTests.cs
@@ -33,12 +33,16 @@ public class UsuarioServiceTests
 
         mockRepo.Setup(repo => repo.Criar(usuario)).ReturnsAsync(usuario);
 
+        _output.WriteLine("********************( Chamando o serviço Criar para o usuário )********************\n");
+
         var resultado = await service.Criar(usuario);
 
         // saber o que houve no teste
-        _output.WriteLine($"Usuário criado: {resultado.Nome}");
+        _output.WriteLine($"Usuário criado: {resultado.Nome}\n");
 
         Assert.Equal(usuario.Nome, resultado.Nome);
+
+        _output.WriteLine("********************( Teste de criação de usuário concluído )********************\n");
     }
 
     /*************************************************************************************************
@@ -61,17 +65,21 @@ public class UsuarioServiceTests
         mockRepo.Setup(repo => repo.ConsultarTodos()).ReturnsAsync(usuariosEsperados);
         var service = new UsuarioService(mockRepo.Object);
 
+        _output.WriteLine("********************( Chamando o serviço Consultar todos os usuários )********************\n");
+
         var resultado = await service.ConsultarTodos();
 
         _output.WriteLine("Usuários retornados:");
         foreach (var user in resultado)
         {
-            _output.WriteLine($"- {user.Nome}");
+            _output.WriteLine($"- {user.Nome}\n");
         }
     
         Assert.Equal(2, resultado.Count);
         Assert.Contains(resultado, u => u.Nome == "Claudio");
         Assert.Contains(resultado, u => u.Nome == "João");
+
+        _output.WriteLine("********************( Teste de consulta de usuário concluído )********************\n");
     }
 
     /*************************************************************************************************
@@ -89,10 +97,14 @@ public class UsuarioServiceTests
         mockRepo.Setup(repo => repo.ConsultarTodos()).ReturnsAsync(new List<Usuario>());
         var service = new UsuarioService(mockRepo.Object);
 
+        _output.WriteLine("********************( Chamando o serviço Consultar lista vazia de usuário )********************\n");
+
         var resultado = await service.ConsultarTodos();
 
-        _output.WriteLine($"Total de usuários retornados: {resultado.Count}");
+        _output.WriteLine($"Total de usuários retornados: {resultado.Count}\n");
         Assert.Empty(resultado);
+
+        _output.WriteLine("********************( Teste de consulta vazia concluído )********************\n");
     }
 
     /*************************************************************************************************
@@ -114,13 +126,133 @@ public class UsuarioServiceTests
         
         var service = new UsuarioService(mockRepo.Object);
 
+        _output.WriteLine("********************( Chamando o serviço Consultar o usuário pelo Id )********************\n");
+
         var resultado = await service.ConsultarId("123");
 
-        _output.WriteLine($"Usuário e Id consultado: {resultado?.Nome}, {resultado?.Id}");
+        _output.WriteLine($"Usuário e Id consultado: {resultado?.Nome}, {resultado?.Id}\n");
         Assert.NotNull(resultado);
         Assert.Equal("Claudio", resultado?.Nome);
         Assert.Equal("123", resultado?.Id);
+
+        _output.WriteLine("********************( Teste de consulta pelo Id concluído )********************\n");
     }
+
+    /*************************************************************************************************
+    
+     Atualizar Usuário
+
+     Teste será realizado apenas para atualizar um usuário existente
+
+    **************************************************************************************************/
+
+    [Fact]
+    public async Task Atualizar_DeveRetornarUsuarioAtualizado()
+    {
+        var mockRepo = new Mock<IUsuarioRepository>();
+        
+        // Usuário original
+        var usuarioOriginal = new Usuario { Nome = "Claudio", Id = "123" };
+        var usuarioAtualizado = new Usuario { Nome = "Claudio Atualizado", Id = "123" };
+        
+        // Configura o mock para retornar o usuário atualizado quando chamado o método de atualização
+        mockRepo.Setup(repo => repo.Atualizar(usuarioOriginal)).ReturnsAsync(usuarioAtualizado);
+        
+        var service = new UsuarioService(mockRepo.Object);
+
+        _output.WriteLine("********************( Chamando o serviço Atualizar todos os dados do usuário )********************\n");
+
+        // Chama o serviço para atualizar o usuário
+        var resultado = await service.Atualizar(usuarioOriginal);
+
+        // Verifica se o nome foi atualizado
+        _output.WriteLine($"Usuário atualizado: {resultado?.Nome}\n");
+        Assert.NotNull(resultado);
+        Assert.Equal("Claudio Atualizado", resultado?.Nome);
+        Assert.Equal("123", resultado?.Id);
+
+        _output.WriteLine("********************( Teste de atualização concluído )********************\n");
+    }
+
+    /*************************************************************************************************
+    
+     Atualizar Parcial Usuário
+
+     Teste será realizado apenas para atualizar parcialmente um usuário existente
+
+    **************************************************************************************************/
+
+    [Fact]
+    public async Task AtualizarParcial_DeveRetornarUsuarioAtualizadoParcialmente()
+    {
+        var mockRepo = new Mock<IUsuarioRepository>();
+        
+        // Usuário original
+        var usuarioOriginal = new Usuario { Nome = "Claudio", Id = "123", Email = "claudio@exemplo.com" };
+         
+        // Nome e Email serão atualizados
+        var usuarioAtualizado = new Usuario { Nome = "Claudio Silva", Id = "123", Email = "claudio.silva@exemplo.com" }; 
+        
+        // Dados de campos a serem atualizados
+        var camposParaAtualizar = new Dictionary<string, object>
+        {
+            { "Nome", "Claudio Silva" },
+            { "Email", "claudio.silva@exemplo.com" }
+        };
+        
+        // Configura o mock para retornar o usuário parcialmente atualizado
+        mockRepo.Setup(repo => repo.AtualizarParcial("123", camposParaAtualizar)).ReturnsAsync(usuarioAtualizado);
+        
+        var service = new UsuarioService(mockRepo.Object);
+
+        _output.WriteLine("********************( Chamando o serviço Atualização parcial do usuário )********************\n");
+
+        // Chama o serviço para atualizar parcialmente o usuário
+        var resultado = await service.AtualizarParcial("123", camposParaAtualizar);
+
+        // Verifica se os campos foram atualizados
+        _output.WriteLine($"Usuário parcialmente atualizado: Nome: {resultado?.Nome}, Email: {resultado?.Email}\n");
+        Assert.NotNull(resultado);
+        Assert.Equal("Claudio Silva", resultado?.Nome);
+        Assert.Equal("claudio.silva@exemplo.com", resultado?.Email);
+        Assert.Equal("123", resultado?.Id);  // ID não foi alterado
+
+        _output.WriteLine("********************( Teste de atualização parcial concluído )********************\n");
+    }
+
+    /*************************************************************************************************
+
+    Excluir Usuário
+
+    Teste será realizado para garantir que o método Excluir seja chamado corretamente
+
+    **************************************************************************************************/
+
+    [Fact]
+    public async Task Excluir_UsuarioValido_ChamaMetodoExcluirNoRepositorio()
+    {
+        var mockRepo = new Mock<IUsuarioRepository>();
+        var usuarioId = "123";  // ID do usuário a ser excluído
+        var service = new UsuarioService(mockRepo.Object);
+
+        // Configurando o mock para simular a exclusão sem retornar nada
+        mockRepo.Setup(repo => repo.Excluir(usuarioId)).Returns(Task.CompletedTask);
+
+        // Chama o método Excluir no serviço
+        await service.Excluir(usuarioId);
+
+        _output.WriteLine("********************( Chamando o serviço Exclusão do usuário )********************\n");
+
+        _output.WriteLine($"Usuário com ID {usuarioId} excluído\n");
+
+        // Verifica se o método Excluir foi chamado uma vez no repositório com o ID correto
+        mockRepo.Verify(repo => repo.Excluir(usuarioId), Times.Once);
+
+        _output.WriteLine("********************( Teste de exclusão concluído )********************\n");
+    }
+
+
+
 
 
 }
